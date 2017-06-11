09c0530c123034eaa077754138a2392b77165bda
Nithin VR <vr@Nithins-MacBook-Air.local>
2017-06-11 18:10:55 +0530

    Adding fp-filter-positions-in-a-list solution

---
+++ HackerRank/FilterPositionsinaList.fs
@@ -0,0 +1,19 @@
+// Hacker Rank Functional Programming Challenges
+// https://www.hackerrank.com/challenges/fp-filter-positions-in-a-list
+//Enter your code here. Read input from STDIN. Print output to STDOUT
+open System
+[<EntryPoint>]
+let main args =
+    let n = Console.ReadLine() |> int
+    let values = 
+        Seq.initInfinite(fun _ -> Console.ReadLine())
+        |> Seq.takeWhile(fun s -> s |> isNull |> not)
+        |> Seq.map int
+        |> Seq.toList
+    let valueToPrint =
+        values
+        |> List.filter (fun x -> x < n)
+
+    for i in valueToPrint do    
+        printfn "%d" i
+    0   
