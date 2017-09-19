e02998af212f574d021602bf937fde594d7692f1
vrnithinkumar <vrnithinkumar@gmail.com>
2017-09-19 22:49:49 +0530

    Fixing method signature

--- MethodTest.fs
+++ MethodTest.fs
@@ -2,6 +2,9 @@ let ReadCase = System.Console.ReadLine >> int
 let x = ReadCase ()
 
 let foo x =
-    let y = x*x
-    sqrt y
-foo 34
+    let y = x * x
+    let a = float y |> sqrt
+    let b = sqrt (float y)
+    b
+ 
+foo 34 |> ignore
